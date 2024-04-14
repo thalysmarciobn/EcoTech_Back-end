305a1289e02ecfdc43178fe4ef7d76b20788429c
Thalys Márcio <thalys@live.com>
2024-04-14 01:24:52 -0300

    Finalização de Design Patterns

--- App/Controladores/TesteControlador.php
+++ App/Controladores/TesteControlador.php
@@ -9,6 +9,7 @@ final class TesteControlador extends BaseControlador
     public function teste(): array
     {
         $this->receptaculo->autenticador->chaveAleatoria(10);
+        
         return $this->responder(['text' => 'Isso é um teste']);
     }
 }
